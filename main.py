2c6189c47bb824f0153e929efcb470fc4eff7468
Paul0522 <paul.milcape@gmail.com>
2023-04-19 22:37:49 -0500

    Modificacion para programacion de los integrantes3

---
+++ main.py
@@ -0,0 +1,12 @@
+# ejemplo de programacion en python
+
+# aqui debe programar henry
+
+
+# aqui debe programar roberth
+
+# aqui debe programar angel
+
+# aqui debe programar paul
+
+## programacion tres
